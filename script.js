f375c8a9ac781e0c9cd6abde3dbef0db81655b8d
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-15 19:38:24 -0700

    Add operator functionality and storing numbers
    Add operator button function so operator can populate results screen.
    Add variables for first and second number to store.
    New query selectors for new HTML elements.

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ const clear = document.querySelector(".clear");
 const equal = document.querySelector(".equal");
 const decimal = document.querySelector(".decimal");
 const results = document.querySelector(".results");
+const resultsTop = document.querySelector(".resultsTop")
+const resultsBot = document.querySelector(".resultsBot")
 
 //Global Variables
 let value = ""; //multiDigit() won't work without this
-
+let op = ""; //Initial value of operator, changes when insertOper() runs
+let firstNum = ""; //Store first number
+let secondNum = ""; //Store second number
 //Operator Functions
 //Add
 const add = (a, b) => {
@@ -43,22 +47,22 @@ const operate = (firstNum, secondNum, operator) => {
 
     if (operator === "+") {
         let answer = add(valueOne, valueTwo);
-        results.innerText = answer;
+        resultsBot.innerText = answer;
     } 
 
     if (operator === "-") {
         let answer = subtract(valueOne, valueTwo); 
-        results.innerText = answer;
+        resultsBot.innerText = answer;
     }  
 
     if (operator === "×") {
         let answer = multiply(valueOne, valueTwo);
-        results.innerText = answer;  
+        resultsBot.innerText = answer;  
     } 
 
     if (operator === "/") {
         let answer = divide(valueOne, valueTwo);
-        results.innerText = answer;
+        resultsBot.innerText = answer;
     } 
 }
 
@@ -67,11 +71,30 @@ numbers.forEach(number => {
     number.addEventListener("click", (e) => {
         //console.log(e, e.target.textContent)
         multiDigit(e.target.textContent);
-        results.textContent = value;
+        resultsBot.textContent = secondNum;
+    })
+})
+
+//Making the calculator work
+operator.forEach(oper => {
+    oper.addEventListener("click", (e) => {
+        insertOper(e.target.textContent);
+        resultsTop.textContent = firstNum;
+        resultsBot.textContent = secondNum;
     })
 })
 
 //Allow for longer numbers instead of single digit
 function multiDigit(num) {
-    value += num;
+    secondNum += num;
+}
+
+//Add an operator into results
+function insertOper(oper) {
+    //console.log(oper);
+    op = oper;
+    firstNum = secondNum + " " + oper;
+    console.log(firstNum);
+    secondNum = "";
+    console.log(secondNum)
 }
