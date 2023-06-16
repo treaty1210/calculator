1ecaea28d0e39362ee8af942d55fac45ecd9470c
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-15 17:35:30 -0700

    Create operate function
    Create operate function that calls upon operators.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const equal = document.querySelector(".equal");
 const decimal = document.querySelector(".decimal");
 const results = document.querySelector(".results");
 
-//Calculator Functions
+//Operator Functions
 //Add
 const add = (a, b) => {
     let answer = a + b;
@@ -30,4 +30,31 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
     let answer = a / b;
     return answer;
+}
+
+//Operate function
+const operate = (firstNum, secondNum, operator) => {
+    //To prevent from adding as a string instead of numbers
+    const valueOne = Number(firstNum);
+    const valueTwo = Number(secondNum);
+
+    if (operator === "+") {
+        let answer = add(valueOne, valueTwo);
+        results.innerText = answer;
+    } 
+
+    if (operator === "-") {
+        let answer = subtract(valueOne, valueTwo); 
+        results.innerText = answer;
+    }  
+
+    if (operator === "×") {
+        let answer = multiply(valueOne, valueTwo);
+        results.innerText = answer;  
+    } 
+
+    if (operator === "/") {
+        let answer = divide(valueOne, valueTwo);
+        results.innerText = answer;
+    } 
 }
